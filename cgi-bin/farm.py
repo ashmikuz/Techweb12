9495916af63cd4d6c75ca540af2497d351a10566
corazza <corazza@cs.unibo.it>
2012-03-24 12:29:44 +0100

    Se l'aggregatore farm riceve una richiesta senza campi e il richiedente accetta xml, gli ritorno il contenuto del file per intero (testato su http://ltw1218.web.cs.unibo.it/aggr-farm)

--- cgi-bin/farm.py
+++ cgi-bin/farm.py
@@ -2,15 +2,23 @@
 
 import cgi
 import os
+import string
 
 
 def main():
     fs = cgi.FieldStorage()
-    print "Content-type: text/plain\n"
-    print os.environ["HTTP_USER_AGENT"]
-    print os.environ["REQUEST_METHOD"]
-    print "qstring=", os.environ["QUERY_STRING"]
-    print os.environ["HTTP_ACCEPT"]
+    accept=os.environ["HTTP_ACCEPT"]
+    if("key" not in fs) and ("comp" not in fs) and ("value" not in fs):
+        if(string.find(accept, "application/xml")):
+            xml=open("../data/farmacieBO2011.xml", "r")
+            print("Content-type: application/xml; charset=UTF-8\n")
+            content=xml.read()
+            print content
+    else:
+        print os.environ["HTTP_USER_AGENT"]
+        print os.environ["REQUEST_METHOD"]
+        print "qstring=", os.environ["QUERY_STRING"]
+        print os.environ["HTTP_ACCEPT"]
 
 
 main()
