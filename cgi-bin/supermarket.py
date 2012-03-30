e67a2bc1ec26ecd460e779b10b1dcf9c406e2e67
corazza <corazza@cs.unibo.it>
2012-03-30 12:20:30 +0200

    Prova filtraEQ

--- cgi-bin/supermarket.py
+++ cgi-bin/supermarket.py
@@ -1,6 +1,50 @@
 #!/usr/bin/python
 
 import cgi
+import error
+import json
+from pprint import  pprint
+
+def _decode_list(data):
+    rv = []
+    for item in data:
+        if isinstance(item, unicode):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = _decode_list(item)
+        elif isinstance(item, dict):
+            item = _decode_dict(item)
+        rv.append(item)
+    return rv
+
+def _decode_dict(data):
+    rv = {}
+    for key, value in data.iteritems():
+        if isinstance(key, unicode):
+           key = key.encode('utf-8')
+        if isinstance(value, unicode):
+           value = value.encode('utf-8')
+        elif isinstance(value, list):
+           value = _decode_list(value)
+        elif isinstance(value, dict):
+           value = _decode_dict(value)
+        rv[key] = value
+    return rv
+
+
+def filtraEQ(key,value,nequal):
+    if(nequal):
+        operator="!="
+    else:
+        operator="=="
+    data=open("../data/supermarketBO2011.json", "r").read()
+    jdata=json.loads(data, object_hook=_decode_dict)
+    print("Content-type: application/json; UTF-8\n")
+    for item in jdata["locations"]:
+        if(item[key]==value):
+            print item
+    print jdata["metadata"]
+    
 
 def main():
     fs = cgi.FieldStorage()
@@ -14,7 +58,7 @@ def main():
         print content
     else:
         if(not chiave) or (not confronto) or (not valore):
-            errhttp("406")
+            error.errhttp("406")
         else:
             if(confronto=="EQ"):
                 filtraEQ(chiave, valore, False)
