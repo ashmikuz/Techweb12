d160ab621000bb5c804dcf0ab3c06f06fa9be61e
corazza <corazza@cs.unibo.it>
2012-04-10 14:21:17 +0200

    Aggiunte costanti per il file csv, aggiunto aggregatore materne, ovviamente golem è down quindi mi son fermato non potendo testare nada...

---
+++ cgi-bin/materne.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -
+
+import os
+import codecs
+import error
+from costanti import uencoding, mimecsv, smaterne
+import cgi
+
+
+def main():
+    fs = cgi.FieldStorage()
+    chiave=fs.getvalue("key")
+    confronto=fs.getvalue("comp")
+    valore=fs.getvalue("value")
+    """
+    questa parte serve per controllare che json in questo caso sia accettato dal descrittore. tuttavia se la abilitiamo adesso, siccome il browser non accetta json non va...
+    la lascio commentata finche non iniziamo con i descrittori
+    if(error.testenviron(os.environ, mimecsv)):
+        return
+    """
+    if(not chiave) and (not confronto) and (not valore):
+        file=open(smaterne, "r")
+        print("Content-type: text/csv; charset=UTF-8\n")
+        content=file.read()
+        print content
+    else:
+        if(not chiave) or (not confronto) or (not valore):
+            error.errhttp("406")
+        else:
+            chiave=chiave.lower().decode(uencoding)
+            confronto=confronto.lower().decode(uencoding)
+            valore=valore.lower().decode(uencoding)
+            if(confronto=="eq"):
+                filtraEQ(chiave, valore, False)
+            elif(confronto=="neq"):
+                filtraEQ(chiave,valore,True)
+            elif(confronto=="contains"):
+                filtraCONTAINS(chiave,valore, False)
+            elif(confronto=="ncontains"):
+                filtraCONTAINS(chiave,valore,True)
+            elif(confronto=="gt"):
+                filtraGT(chiave,valore,True, False)
+            elif(confronto=="lt"):
+                filtraGT(chiave,valore,False,False)
+            elif(confronto=="ge"):
+                filtraGT(chiave,valore,True,True)
+            elif(confronto=="le"):
+                filtraGT(chiave,valore,False,True)
+            else:
+                error.errcode("406")
+                
+            
+
+main()
