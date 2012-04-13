fc77dcda2bb67996bbe288608d547212a5bac0ec
corazza <corazza@cs.unibo.it>
2012-04-13 17:20:05 +0200

    Finito aggregatore csv

--- cgi-bin/materne.py
+++ cgi-bin/materne.py
@@ -6,7 +6,91 @@ import codecs
 import error
 from costanti import uencoding, mimecsv, smaterne
 import cgi
+import csv
 
+ops = {
+  "==": operator.eq,
+  "!=": operator.ne,
+  "<": operator.lt,
+  "<=": operator.le,
+  ">": operator.gt,
+  ">=": operator.ge,
+  "not": operator.not_,
+  "id": operator.truth
+  }
+
+dial=csv.Dialect
+dial.quoting=csv.QUOTE_ALL
+dial.quotechar='"'
+dial.delimiter=","
+dial.lineterminator="\n"
+dial.escapechar="\\"
+
+def filtraEQ(key,value,nequal):
+    if(nequal):
+        op="!="
+    else:
+        op="=="
+    if(key != "id" and key != "name" and key != "lat,long" and key != "category"):
+        error.errcode("406")
+        return
+    if(key=="lat,long"):
+        lat,longi=value.split(",")
+    else:
+        key=key[0].upper()+key[1:]
+    data=open(smaterne,"r")
+    orig=csv.DictReader(data)
+    result=csv.DictWriter(sys.stdout, orig.fieldnames, dialect=dial)
+    print("Content-type: text/csv; charset=UTF-8\n")
+    result.writeheader()
+    for item in orig:
+        if(key!=u"lat,long" and ops[op](item[key].lower(), value)):
+            result.writerow(item)
+        elif(ops[op](item["Lat"], lat) and (ops[op](item["Long"], longi))):
+            result.writerow(item)
+            
+            
+def filtraCONTAINS(key,value,ncontains):
+    if(ncontains):
+        op="not"
+    else:
+        op="id"
+    if(key != "id" and key != "name" and key != "category" and key != "address" and key != "opening" and key != "closing"):
+        error.errcode("406")
+        return
+    if(ncontains and key == "address"):
+        error.errcode("406")
+        return
+    key=key[0].upper()+key[1:]
+    data=open(smaterne,"r")
+    orig=csv.DictReader(data)
+    result=csv.DictWriter(sys.stdout, orig.fieldnames, dialect=dial)
+    print("Content-type: text/csv; charset=UTF-8\n")
+    result.writeheader()
+    for item in orig:
+        if(ops[op](value in item[key].lower())):
+            result.writerow(item)    
+    
+def filtraGT(key,value,greaterthan, equal):
+    if(greaterthan):
+        op=">"
+    else:
+        op="<"
+    if(not equal):
+        op+="="
+    if(key!="lat,long"):
+        error.errcode("406")
+        return
+    lat,longi=value.split(",")
+    data=open(smaterne,"r")
+    orig=csv.DictReader(data)
+    result=csv.DictWriter(sys.stdout, orig.fieldnames, dialect=dial)
+    print("Content-type: text/csv; charset=UTF-8\n")
+    result.writeheader()
+    for item in orig:
+      if(ops[op](float(item["Lat"]), float(lat)) and (ops[op](float(item["Long"]), float(longi)))):
+          result.writerow(item)
+      
 
 def main():
     fs = cgi.FieldStorage()
