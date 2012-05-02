16ee883ac82754a4fe4245134b848ddd0d8dbd67
corazza <corazza@cs.unibo.it>
2012-05-02 18:15:13 +0200

    Lavori su metacatalogo e aggr

--- cgi-bin/trasforma.py
+++ cgi-bin/trasforma.py
@@ -3,7 +3,7 @@
 
 import sys
 from math import radians, sqrt, atan2, cos, fabs, sin, pow
-from costanti import uencoding
+from costanti import uencoding, csvfields
 import error
 import csv
 import json
@@ -15,9 +15,10 @@ from collections import OrderedDict
 raggioterra=float(6371009)
 
 class location:
-    def __init__(self, id, category, name, lat, long, address, opening, closing, tel, note):
+    def __init__(self, id, category,subcategory, name, lat, long, address, opening, closing, tel, note):
         self.id=id
         self.category=category
+        self.subcategory=subcategory
         self.name=name
         self.lat=lat
         self.long=long
@@ -40,6 +41,15 @@ class location:
         angle=atan2(x,y)
         self.distance=angle*raggioterra
 
+global meta
+
+class metadata:
+    def __init__(self, creator, created, version, source, valid):
+        self.creator=creator
+        self.created=created
+        self.version=version
+        self.source=source
+        self.valid=valid
       
 def getopening(loc):
     result=""
@@ -51,9 +61,20 @@ def getopening(loc):
 def locationfromxml(data,ellist):
     xml=etree.fromstring(data)
     res=xml.xpath("/locations/location")
+    metad=xml.xpath("/locations/metadata")
+    creator=metad[0].find("creator").text
+    created=metad[0].find("created").text
+    version=metad[0].find("version").text
+    source=metad[0].find("source").text
+    valid=metad[0].find("valid").text
+    meta=metadata(creator,created, version,source,valid)
     for element in res:
         attr=element.attrib
         id=attr["id"]
+        if(element.find("subcategory")is not None):
+            subcategory=(element.find("subcategory").text)
+        else:
+            subcategory=""
         category=(element.find("category").text)
         name=(element.find("name").text)
         lat=(attr["lat"])
@@ -69,8 +90,9 @@ def locationfromxml(data,ellist):
             note=(element.find("note").text)
         else:
             note=""
-        loc=location(id, category, name, lat, long, address, opening, closing, tel, note)
+        loc=location(id, category,subcategory ,name, lat, long, address, opening, closing, tel, note)
         ellist.append(loc)
+    return meta
 
 def locationfromjson(data,ellist):
     orig=json.loads(data)
@@ -79,9 +101,11 @@ def locationfromjson(data,ellist):
         for subkey,subval in value.iteritems():
             if(key!="metadata"):
                 id=subkey
-                category=[]
-                for item in subval["category"]:
-                    category.append(item)
+                category=subval["category"][0]
+                if len(subval["category"]==2):
+                    subcategory=subval["category"][1]
+                else:
+                    subcategory=""
                 name=subval["name"]
                 lat=(subval["lat"])
                 long=(subval["long"])
@@ -96,14 +120,33 @@ def locationfromjson(data,ellist):
                     note=subval["note"]
                 else:
                     note=""
-                loc=location(id, category, name, lat, long, address, opening, closing, tel, note)
+                loc=location(id, category,subcategory, name, lat, long, address, opening, closing, tel, note)
                 ellist.append(loc)
+            else:
+             	creator=value["creator"]
+             	created=value["created"]
+             	valid=value["valid"]
+             	version=value["version"]
+             	source=value["source"]
+             	meta=metadata(creator,created,version,source,valid)
+	return meta
+             	
 
 def locationfromcsv(data,loclist):
     orig=csv.DictReader(data)
+    donemetad=False
     for item in orig:
+     	if(donemetad!=True):
+			 creator=item["creator"]
+			 created=item["created"]
+			 valid=item["valid"]
+			 version=item["version"]
+			 source=item["source"]
+			 donemetad=True
+			 meta=metadata(creator,created,version,source,valid)
         id=item["Id"]
         category=item["Category"]
+        subcategory=item["subcategory"]
         name=item["Name"]
         lat=(item["Lat"])
         long=(item["Long"])
@@ -118,14 +161,30 @@ def locationfromcsv(data,loclist):
             note=item["note"]
         else:
             note=""
-        loc=location(id, category, name, lat, long, address, opening, closing, tel, note)
+        loc=location(id, category, subcategory,name, lat, long, address, opening, closing, tel, note)
         ellist.append(loc)
+  	return meta
 
-def locationtoxml(ellist):
+def locationtoxml(ellist,meta):
     root=etree.Element("locations")
+    md=etree.SubElement(root,"metadata")
+    submd=etree.SubElement(md,"creator")
+    submd.text=meta.creator
+    submd=etree.SubElement(md,"created")
+    submd.text=meta.created
+    submd=etree.SubElement(md,"version")
+    submd.text=meta.version
+    submd=etree.SubElement(md,"source")
+    submd.text=meta.source
+    submd=etree.SubElement(md,"valid")
+    submd.text=meta.valid
     for location in ellist:
         child=etree.SubElement(root,"location" , lat=location.lat, id=location.id, long=location.long)
         subchild=etree.SubElement(child, "category")
+        subchild.text=location.category
+        if(location.subcategory!=""):
+            subchild=etree.SubElement(child, "subcategory")
+            subchild.text=location.subcategory
         subchild.text=location.category
         subchild=etree.SubElement(child, "name")
         subchild.text=location.name
@@ -145,18 +204,27 @@ def locationtoxml(ellist):
     print etree.tostring(root, pretty_print=True, xml_declaration=True, doctype='<!DOCTYPE locations SYSTEM "http://vitali.web.cs.unibo.it/twiki/pub/TechWeb12/DTDs/locations.dtd">',  encoding=uencoding)
     return
 
-def locationtojson(ellist):
-    root= {"locations" : OrderedDict()}
+def locationtojson(ellist, meta):
+    md={}
+    md["creator"]=meta.creator
+    md["created"]=meta.created
+    md["version"]=meta.version
+    md["source"]=meta.source
+    md["valid"]=meta.valid
+    root= {"locations" : OrderedDict(),
+		  	"metadata" : md}
     for location in ellist:
         cur={}
         cur.update({"name": location.name})
-        cur.update({"category" : location.category})
+        cur.update({"category" : []})
+        cur["category"].append(location.category)
+        if(location.subcategory!=""):
+            cur["category"].append(location.subcategory)
         cur.update({"address" : location.address})
         cur.update({"lat" : location.lat})
         cur.update({"long" : location.long})
         cur.update({"opening": location.opening})
         cur.update({"closing" : location.closing})
-        cur.update({"category" : location.category})
         if(location.note!=""):
             cur.update({"note" : location.note})
         if(location.tel!=""):
@@ -166,20 +234,23 @@ def locationtojson(ellist):
     print json.dumps(root, ensure_ascii=False, encoding=uencoding ,sort_keys=False, indent=4).encode(uencoding)            
     return
 
-
+def locationtocsv(ellist,meta):
     print("Content-type: text/plain; charset=UTF-8\n")
-    print json.dumps(root, ensure_ascii=False, encoding=uencoding ,sort_keys=False, indent=4).encode(uencoding)            
-    return
+    print csvfields
+    for location in ellist:
+        str= "\""+location.id+"\",\""+location.category+"\",\""+location.name+"\",\""+location.address+"\",\""+location.lat+"\",\""+location.long+"\",\""\
+        +location.subcategory+"\",\""+location.note+"\",\""+location.opening+"\",\""+location.closing+"\",\""+meta.creator+"\",\""+meta.created+"\",\""+meta.valid+"\",\""+meta.source+"\",\""
+        print str
 
-def formatresult(mimetype, ellist):
+def formatresult(mimetype, ellist,meta):
     if (False and "application/xml" in mimetype):  
-        locationtoxml(ellist)
-    elif(True or "application/json" in mimetype):
-        locationtojson(ellist)
-    elif("text/csv" in mimetype):
-        locationtocsv(ellist)
+        locationtoxml(ellist,meta)
+    elif(False and "application/json" in mimetype):
+        locationtojson(ellist,meta)
+    elif(True or "text/csv" in mimetype):
+        locationtocsv(ellist,meta)
     elif("text/turtle" in mimetype):
-        locationtoturtle(ellist)
+        locationtoturtle(ellist,meta)
     else:
         error.errhttp("406")
 
--- cgi-bin/vicinoa.py
+++ cgi-bin/vicinoa.py
@@ -7,6 +7,8 @@ import codecs
 import urllib2
 import os
 import trasforma
+from trasforma import metadata
+from aggrutils import getaggrurl
 
 headers={'Accept': 'application/xml,application/json,text/turtle, text/csv, text/html, text/plain;'}
 
@@ -21,8 +23,10 @@ def main():
     if ((not aggr) or (not lat) or (not longi)):
         error.errhttp("406")
     else:
-        """urlaggr=getaggrurl(aggr)"""
-        urlaggr="http://ltw1218.web.cs.unibo.it/ltw1218-farmacie"
+        urlaggr=getaggrurl(aggr)
+        if(urlaggr=="404"):
+            error.errhttp("404")
+            return
         req=urllib2.Request(url=urlaggr)
         req.add_header('Accept', 'application/xml, text/turtle, text/csv, application/json')
         response = urllib2.urlopen(req)
@@ -30,18 +34,18 @@ def main():
         resource=response.read()
         response.close()
         if(restype=="application/xml"):
-            trasforma.locationfromxml(resource,loclist)
+            meta=trasforma.locationfromxml(resource,loclist)
         elif(restype=="text/turtle"):
-            trasforma.locationfromturtle(resource,loclist)
+            meta=trasforma.locationfromturtle(resource,loclist)
         elif(restype=="text/csv"):
-            trasforma.locationfromcsv(resource,loclist)
+            meta=trasforma.locationfromcsv(resource,loclist)
         elif(restype=="application/json"):
-            trasforma.locationfromcsv(resource,loclist)
+            meta=trasforma.locationfromcsv(resource,loclist)
         else:
             error.errhttp("406")
         computedistances(loclist, lat, longi)
         loclist.sort(key=lambda location: location.distance)
-        trasforma.formatresult(os.environ["HTTP_ACCEPT"], loclist)
+        trasforma.formatresult(os.environ["HTTP_ACCEPT"], loclist, meta)
         
 def computedistances(list, lat, longi):
     for location in list:
