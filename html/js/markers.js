1dae9eb0e4c46a59b2d658d2204e18c7ec9a2e7d
corazza <corazza@cs.unibo.it>
2012-05-15 18:13:53 +0200

    prove markers.js...

--- html/js/markers.js
+++ html/js/markers.js
@@ -1,11 +1,38 @@
 
-function drawmarkers(checkbox)
+var aggregatori = new Array();
+aggregatori['farmacie'] = "ltw1218-farmacie";
+aggregatori['medici'] = "ltw1218-medici";
+aggregatori['supermarket'] = "ltw1218-supermarket";
+aggregatori['poste'] = "ltw1218-poste";
+aggregatori['materne']= "ltw1218-materne"
+
+function drawmarkers()
 {
-	if(checkbox.checked)
-		{
-			aggrurl=getaggr(checkbox.id);
-			prova=document.getElementById("prova");
-			prova.appendChild(document.createTextNode("ammaccabanane"));
-		}
+	xml=getxml()
 }
 
+function getxml()
+{
+	var xmlretval=new XpathResult();
+	var xmlDoc=document.implementation.createDocument("","",null);
+	xmlDoc.async=false;
+	var checkboxes=new Array();
+	checkboxes[0]=document.getElementById("farmacie");
+	checkboxes[1]=document.getElementById("materne");
+	checkboxes[2]=document.getElementById("poste");
+	checkboxes[3]=document.getElementById("supermarket");
+	checkboxes[4]=document.getElementById("medici");
+	
+	for (i=0; i<5; i++)
+		{
+			if(checkboxes[i].checked)
+				{
+					urlaggr="http://ltw1218.web.cs.unibo.it/ltw1218-"+checkboxes[i].id;					
+					aggrxml=xmlDoc.load(urlaggr)
+					document.evaluate("/locations/location", aggrxml,null ,XPathResult.ANY_TYPE, xmlretval);
+				}
+				
+		}
+	return retval;
+	
+}
